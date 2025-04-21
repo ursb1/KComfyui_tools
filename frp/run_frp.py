9d43bdc33fcfd392753f3df6c975b44a6439db86
ursb1 <uurrssbb01@outlook.com>
2025-04-21 23:29:47 +0800

    fix

--- frp/run_frp.py
+++ frp/run_frp.py
@@ -1,17 +1,10 @@
 import subprocess
 
-process1 = subprocess.Popen(
-    ["./res/frpc", "-c", "./res/frpc1145.ini"],
+process = subprocess.Popen(
+    ["./res/frpc", "-c", "./res/frpc.ini"],
     stdout=subprocess.DEVNULL,  # 将标准输出重定向到 DEVNULL防止显示
     stderr=subprocess.DEVNULL,  # 将标准错误重定向到 DEVNULL
     start_new_session=True
 )
-process2 = subprocess.Popen(
-    ["./res/frpc", "-c", "./res/frpc8080.ini"],
-    stdout=subprocess.DEVNULL,  # 将标准输出重定向到 DEVNULL防止显示
-    stderr=subprocess.DEVNULL,  # 将标准错误重定向到 DEVNULL
-    start_new_session=True
-)
-print("访问1145端口：http://hk-6.lcf.im:51145")
-print("访问8080端口：http://hk-6.lcf.im:58080")
-print("进程管理：process1-1145，process2-8080")
+print("访问1145端口：sj.frp.one:51145")
+print("访问8080端口：sj.frp.one:58080")
